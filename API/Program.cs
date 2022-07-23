f61b92c9fcc96c7b93039d1735bf7e5d31610a94
Chad <chad.lines1@gmail.com>
2022-07-23 10:31:29 -0600

    0.004 - Creating the Database and Migration Service

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -13,7 +15,27 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // This will host any services that we create within Services.CreateScope
+            using var scope = host.Services.CreateScope(); 
+            var services = scope.ServiceProvider;
+
+            // If a database doesn't exist, we'll try to create it. If it fails
+            // then we log the failure
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            // Start the application
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
